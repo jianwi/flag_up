6b95687b53d41e17007779dfc536ceadcb1b6c18
dujianjunn <v@jianwi.cn>
2020-09-29 00:14:10 +0800

    服务器端

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const http = require("http")
+const redis = require('redis');
+const client = redis.createClient( 6379, '127.0.0.1');
+
+
+const server = http.createServer((req,res)=>{
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain;charset=utf-8')
+    res.setHeader("Server",'Nginx pro 2021')
+    if (/num/.test(req.url)){
+        client.get('num',function (erro,data){
+            res.end(data)
+        })
+    }else if (/add/.test(req.url)){
+        client.incr('num',function (err,data){
+            console.log(data)
+            res.end("ok")
+        })
+    }else {
+        res.statusCode = 404
+        res.end("404 undefined")
+    }
+})
+server.listen("3300",()=>{
+    console.log('服务器运行')
+})
